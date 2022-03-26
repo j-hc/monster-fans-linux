e0337ab150d8b1a3ea8677d22f14d6334b3e7f18
j-hc <karayay12@gmail.com>
2022-03-26 21:44:26 +0300

    make quiet mode optional

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,6 @@ const EC_REG_SIZE: usize = 0x100;
 const EC_REG_FAN_DUTY: usize = 0xCE;
 const EC_REG_CPU_TEMP: usize = 0x07;
 
-#[allow(dead_code)]
 fn calc_next_duty(temp: f32) -> f32 {
     if temp <= 40.0 {
         32.0
@@ -34,27 +33,39 @@ fn calc_next_duty_quiet(temp: f32) -> f32 {
     if temp <= 40.0 {
         30.0
     } else if temp <= 60.0 {
-        0.5 * temp + 10.0
+        0.6 * temp + 10.0
     } else if temp <= 80.0 {
-        1.8 * temp - 54.0
-    } else if temp <= 90.0 {
-        2.2 * temp - 100.0
+        1.6 * temp - 46.0
+    } else if temp <= 85.0 {
+        2.2 * temp - 105.0
     } else {
         100.0
     }
 }
 
-#[derive(Default)]
-pub struct EC {
+pub struct EC<'a> {
     pub fan_duty: u8,
+    pub fan_next_duty: u16,
     pub cpu_temp: u8,
+    pub duty_calc_func: &'a dyn Fn(f32) -> f32,
     i: u8,
     i2: u8,
 }
-impl EC {
+impl<'a> EC<'a> {
     const MAX_STEP: u16 = 8;
     const LOWER_END: u16 = 10;
     const HIHGER_END: u16 = 5;
+
+    pub fn new(duty_calc_func: &'a dyn Fn(f32) -> f32) -> Self {
+        Self {
+            fan_duty: 0,
+            fan_next_duty: 0,
+            cpu_temp: 0,
+            duty_calc_func,
+            i: 0,
+            i2: 0,
+        }
+    }
 
     pub fn read_from_kernel(&mut self) -> io::Result<()> {
         let mut f = File::open("/sys/kernel/debug/ec/ec0/io")?;
@@ -66,7 +77,7 @@ impl EC {
     }
 
     pub fn switch_to_next_duty(&mut self) -> Option<bool> {
-        let fan = calc_next_duty_quiet(self.cpu_temp as f32) as u16;
+        let fan = (self.duty_calc_func)(self.cpu_temp as f32) as u16;
         let current_fd = self.fan_duty as u16;
 
         if self.i >= 4
@@ -75,7 +86,7 @@ impl EC {
             self.i = 0;
             let next_duty: u16;
 
-            if current_fd == fan || fan.abs_diff(current_fd) <= 3 {
+            if fan.abs_diff(current_fd) <= 3 {
                 return None;
             } else if fan > current_fd {
                 next_duty = fan;
@@ -87,7 +98,7 @@ impl EC {
                 self.i2 += 1;
                 return None;
             }
-
+            self.fan_next_duty = next_duty;
             return Some(ec_write_fan_duty(next_duty as f32));
         }
         self.i += 1;
@@ -103,13 +114,46 @@ impl EC {
     }
 }
 
+enum Mode {
+    Default,
+    Quiet,
+}
+impl std::fmt::Display for Mode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Mode::Default => write!(f, "default mode"),
+            Mode::Quiet => write!(f, "quiet mode"),
+        }
+    }
+}
+
 fn main() {
-    set_handlers();
+    const DEFAULT_MODE_ARG: &str = "--default";
+    const QUIET_MODE_ARG: &str = "--quiet";
 
     if !ec_init() {
-        eprintln!("run with sudo");
+        eprintln!("run with sudo!");
         process::exit(1);
     }
+
+    let mut args = std::env::args();
+    let executable_name = args.next().unwrap();
+
+    let mode = match args.next() {
+        Some(arg) => match arg.as_str() {
+            QUIET_MODE_ARG => Mode::Quiet,
+            DEFAULT_MODE_ARG => Mode::Default,
+            _ => {
+                eprintln!("Usage:\n{executable_name} {DEFAULT_MODE_ARG}|{QUIET_MODE_ARG}");
+                process::exit(1);
+            }
+        },
+        None => Mode::Default,
+    };
+
+    println!("Running in {}", mode);
+
+    set_handlers();
 
     println!(
         "initial: fan={}%, CPU={}°C",
@@ -118,7 +162,10 @@ fn main() {
     );
 
     EC::load_module().unwrap();
-    let mut ec = EC::default();
+    let mut ec = match mode {
+        Mode::Default => EC::new(&calc_next_duty),
+        Mode::Quiet => EC::new(&calc_next_duty_quiet),
+    };
 
     ec.read_from_kernel().unwrap();
     println!("initial ec: fan={}%, CPU={}°C", ec.fan_duty, ec.cpu_temp);
@@ -131,7 +178,10 @@ fn main() {
 
         if let Some(s) = ec.switch_to_next_duty() {
             if s {
-                println!("next: fan={}%, CPU={}°C", ec.fan_duty, ec.cpu_temp);
+                println!(
+                    "current: fan={}%, CPU={}°C, next: fan={}%",
+                    ec.fan_duty, ec.cpu_temp, ec.fan_next_duty
+                );
             } else {
                 eprintln!("err on writing to the ec fan duty");
                 break;
